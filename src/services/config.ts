f3ad1ecd81d367d1ba95bbe656b056fabdbd261a
hemantsingh0557 <hemantsingh0557@gmail.com>
2025-02-28 14:04:23 +0530

    correct url path

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,7 +1,7 @@
 const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://google-sheet-server.vercel.app/";
+    : "https://google-sheet-server.vercel.app";
 
 export const USER_URL = `${BASE_URL}/api/user`;
 export const SHEET_URL = `${BASE_URL}/api/sheet`;
